7b3f0f72c5ce1d140cd0a4a976c0f8183b0f6491
christoph ott <christoph.ott@lean-coders.at>
2023-12-03 02:07:04 +0100

    remove targets

--- aoc_day2/target/debug/aoc_day2.d
+++
@@ -1,1 +0,0 @@
-/home/uter/src/adventofcode/2023/aoc_day2/target/debug/aoc_day2: /home/uter/src/adventofcode/2023/aoc_day2/src/main.rs
--- aoc_day2/target/debug/deps/aoc_day2-a54e2be64629539c.d
+++
@@ -1,5 +0,0 @@
-/home/uter/src/adventofcode/2023/aoc_day2/target/debug/deps/libaoc_day2-a54e2be64629539c.rmeta: src/main.rs
-
-/home/uter/src/adventofcode/2023/aoc_day2/target/debug/deps/aoc_day2-a54e2be64629539c.d: src/main.rs
-
-src/main.rs:
