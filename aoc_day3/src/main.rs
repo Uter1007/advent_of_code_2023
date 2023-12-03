a79af6419071d2e207a8f432c40fff6f2bb0ca26
christoph ott <christoph.ott@lean-coders.at>
2023-12-03 14:36:06 +0100

    add part3

--- aoc_day3/src/main.rs
+++ aoc_day3/src/main.rs
@@ -15,11 +15,12 @@ struct Token {
 
 fn main() {
     let mut endresult:i32 = 0;
+    let mut endresult2:i32 = 0;
     let re = Regex::new(r"[0-9]+|[+@#&*$=\/%\-]+").unwrap();
     let mut game_results: Vec<Token> = Vec::new();
     let mut line_num = 0;
     // File input.txt must exist in the current path
-    if let Ok(lines) = read_lines("./input3.txt") {
+    if let Ok(lines) = read_lines("./input.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
@@ -117,6 +118,49 @@ fn main() {
         }
 
         println!("Endresult: {}", endresult);
+
+        for game in &game_results {
+            let mut was_found = false;
+
+            if game.is_symbol && game.token == "*" {
+
+                println!("Game: {:?} => L:{} C:{}-{}", game.token, game.line_number, game.position.0, game.position.1);
+
+                let current_line_number = game.line_number;
+                let mut current_min_position = game.position.0;
+                let mut current_max_position = game.position.1;
+
+                let mut current_line_number_min = game.line_number;
+                let mut current_line_number_max = game.line_number;
+
+                if current_line_number_min > 0 {
+                    current_line_number_min -= 1;
+                }
+
+                current_line_number_max += 1;
+
+                // println!("Game: {:?} => L:{}-{} C:{}-{}", game.token, current_line_number_min, current_line_number_max, current_min_position, current_max_position);
+
+                let found_tokens: Vec<_> = game_results
+                    .iter()
+                    .filter(|&token| {
+                        (token.position.0 <= current_max_position && token.position.1 >= current_min_position)
+                        && token.is_symbol == false
+                        && token.line_number <= current_line_number_max
+                        && token.line_number >= current_line_number_min
+                    })
+                    .collect();
+
+                if found_tokens.len() == 2 { 
+                   //println!("Game: {:?} => Gefundene Tokens: {:?}", game ,found_tokens);
+                   let result = found_tokens[0].result_as_number * found_tokens[1].result_as_number;
+                   endresult2 += result;
+                }
+            }
+        }
+
+        println!("Endresult2: {}", endresult2);
+
     }
 
 }
